498c1875a4e5b00512d9d622bb1a8a6c6dfeff14
Sam Bevan <samueleganbevan@gmail.com>
2020-05-28 03:00:25 -0400

    added basic styling to edit and new pages

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -1,44 +1,36 @@
 const React = require('react');
+const Layout = require('./components/Layout');
 
 class Edit extends React.Component {
     render() {
         const { job } = this.props;
         return (
-            <div>
-                <h1>Edit Job Info</h1>
-                <form
-                    action={`/jobs/edit/${job._id}?_method=put`}
+            <Layout>
+                <div className="edit-page">
+                    <h1>Edit {job.company} Listing</h1>
+                </div>
+                <div className="edit-main-container">
+                    <form action={`/jobs/edit/${job._id}?_method=put`}
                     method="POST"
-                >
-                    Company: <input type="text" name="company" value={job.company} /> <br />
-                    Application Complete?:{' '}
-                    <input
-                        type="checkbox"
-                        checked={job.applicationComplete ? 'checked' : ''}
-                        name="applicationComplete"
-                    />
-                    <br />
-                    Status (Check for Active):{' '}
-                    <input
-                        type="checkbox"
-                        checked={job.isActive ? 'checked' : ''}
-                        name="isActive"
-                    />
-                    <br />
-                    Date Posted:<input type="text" name="interviewDate" value={job.interviewDate} />
-                    <br />
-                    Hiring Manager:<input type="text" name="interviewer" value={job.interviewer} />
-                    <br />
-                    Notes:<input type="text" name="notes" value={job.notes} />
-                    <br />
-                    Company Logo (url):<input type="text" name="img" value={job.img} />
-
-                    <input type="submit" name="" value="Edit Job" />
-
-
-
-                </form>
-            </div>
+                    >
+                        Company: <input type="text" name="company" value={job.company} /> <br />
+                        Application Complete?:{' '}
+                        <input type="checkbox" checked={job.applicationComplete ? 'checked' : ''}
+                        name="applicationComplete"/><br />
+                        Status (Check for Active):{' '}
+                        <input type="checkbox" checked={job.isActive ? 'checked' : ''}
+                        name="isActive"/><br />
+                        Date Posted:<input type="text" name="interviewDate" value={job.interviewDate} />
+                        <br />
+                        Hiring Manager:<input type="text" name="interviewer" value={job.interviewer} />
+                        <br />
+                        Notes:<input type="text" name="notes" value={job.notes} />
+                        <br />
+                        Company Logo (url):<input type="text" name="img" value={job.img} /><br/>
+                        <input type="submit" name="" value="Edit Job" />
+                    </form>
+                </div>
+            </Layout>  
         );
     }
 }
--- views/New.jsx
+++ views/New.jsx
@@ -1,21 +1,26 @@
 const React = require('react');
+const Layout = require('./components/Layout');
 
 class New extends React.Component {
     render() {
       return (
-          <div>
-              <h1>New Job</h1>
-              <form action="/jobs" method="POST">
-                    Company: <input type="text" name="company" /><br/>
-                    Application Status (Check if Active): <input type="checkbox" name="isActive"/><br/>
-                    Application Complete (Check if Complete): <input type="checkbox" name="applicationComplete"/><br/>
-                    Interview Date: <input type="text" name="interviewDate" /><br/>
-                    Hiring Manager: <input 
-                    type="text" name="interviewer" /><br/>
-                    Notes: <input type="text" name="notes" /><br/>
+        <Layout>
+          <div className="new-page">
+            <h1>New Job</h1>
+          </div>
+          <div className="new-main-container">
+            <form action="/jobs" method="POST">
+                  Company: <input type="text" name="company" /><br/>
+                  Application Status (Check if Active): <input type="checkbox" name="isActive"/><br/>
+                  Application Complete (Check if Complete): <input type="checkbox" name="applicationComplete"/><br/>
+                  Interview Date: <input type="text" name="interviewDate" /><br/>
+                  Hiring Manager: <input type="text" name="interviewer" /><br/>
+                  Company Logo (url): <input type="text" name="img" /><br/>
+                  Notes: <input type="text" name="notes" /><br/>
                   <input type="submit" name="" value="Create Job Entry"/>
-               </form>
-          </div>);
+              </form>
+          </div>
+        </Layout>)
     }
   }
   
