001ed10d8cde1a05dbe256ed2c3b5293374fd8d5
Sam Bevan <samueleganbevan@gmail.com>
2020-05-28 05:08:29 -0400

    changes

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const methodOverride = require('method-override');
 const jobsController = require('./controllers/jobs');
 require('dotenv').config(); 
 const db = mongoose.connection;
+const PORT = process.env.PORT || 3000;
 
 
-const MONGODB_URI =  `mongodb://localhost:/jobjournal`;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `jobjournal`;
 
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true, useUnifiedTopology: true });
@@ -27,6 +28,7 @@ db.on('open' , ()=>{});
 // middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(methodOverride('_method'));
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
@@ -36,6 +38,6 @@ app.use('/jobs', jobsController);
 
 
 // listen
-app.listen(process.env.PORT || 3000, () => {
+app.listen(PORT, () => {
     console.log('listening on: 3000' );
 });
